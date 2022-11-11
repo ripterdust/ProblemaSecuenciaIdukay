96486a1cb9083c6c4da41dce252ca5bc0d121ef6
Ripterdust <bryantello2010@hotmail.com>
2022-11-11 15:54:41 -0600

    :rocket: Formateado el código

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 export const calculateTerm = (position: number): number => {
     const sequence: number[] = [1, 2, 3, 4]
+
     const fixedLenght = sequence.length - 1
 
     if (position <= fixedLenght) {
         return sequence[position]
     }
+
     const numberOfConsecutive: number = 4
+
     const residue: number = (position % numberOfConsecutive) + 1
+
     const moduleOfPosition: number = Math.trunc(position / numberOfConsecutive)
     const doubleModule: number = moduleOfPosition * 2 + residue
+
     const floatDifference: number = position / doubleModule
     const difference: number = floatDifference > 0 ? Math.trunc(floatDifference) : 1
 
