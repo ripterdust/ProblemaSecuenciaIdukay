33b6d636c9a901450fda9935119f5cba4137c24c
Ripterdust <bryantello2010@hotmail.com>
2022-11-11 14:38:07 -0600

    :rocket: Pasadas todas las pruebas menos la última

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ export const calculateTerm = (position: number): number => {
     const numberOfConsecutive = 4
     let residue = (position % numberOfConsecutive) + 1
 
-    const dividedByConsecutive = parseInt(`${position / numberOfConsecutive}`)
-    const result = dividedByConsecutive * 2 + residue
+    const moduleOfPosition = parseInt(`${position / numberOfConsecutive}`)
+    const doubleModule = moduleOfPosition * 2 + residue
+
+    const floatDifference = position / doubleModule
+    const difference = floatDifference > 0 ? Math.trunc(floatDifference) : 1
+    const result = doubleModule * difference
 
     return result
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { calculateTerm } from './app'
 
-const inputs = [0, 1, 5, 76, 545421, 87123641123172368]
+const inputs = [0, 5, 76, 545421, 87123641123172368]
 
-inputs.map((input: number) => {
-    calculateTerm(input)
+inputs.map((input: number, key: number) => {
+    const result = calculateTerm(input)
+    console.table({ input, result })
 })
