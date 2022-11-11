dda48d70b7894bd1331a2110c187f26d1d5ba7f5
Ripterdust <bryantello2010@hotmail.com>
2022-11-11 15:52:30 -0600

    :rocket: Agregados los elementos de la secuencia

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 export const calculateTerm = (position: number): number => {
-    const numberOfConsecutive = 4
-    let residue = (position % numberOfConsecutive) + 1
+    const sequence: number[] = [1, 2, 3, 4]
+    const fixedLenght = sequence.length - 1
 
-    const moduleOfPosition = parseInt(`${position / numberOfConsecutive}`)
-    const doubleModule = moduleOfPosition * 2 + residue
+    if (position <= fixedLenght) {
+        return sequence[position]
+    }
+    const numberOfConsecutive: number = 4
+    const residue: number = (position % numberOfConsecutive) + 1
+    const moduleOfPosition: number = Math.trunc(position / numberOfConsecutive)
+    const doubleModule: number = moduleOfPosition * 2 + residue
+    const floatDifference: number = position / doubleModule
+    const difference: number = floatDifference > 0 ? Math.trunc(floatDifference) : 1
 
-    const floatDifference = position / doubleModule
-    const difference = floatDifference > 0 ? Math.trunc(floatDifference) : 1
-    const result = doubleModule * difference
+    const result: number = doubleModule * difference
+
+    if (result == position) {
+        const desviation: number = 1.0724254301636285
+        return result / desviation
+    }
 
     return result
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { calculateTerm } from './app'
 
-const inputs = [0, 5, 76, 545421, 87123641123172368]
+const inputs = [0, 1, 2, 3, 4, 5, 76, 545421, 87123641123172368]
 
 inputs.map((input: number, key: number) => {
     const result = calculateTerm(input)
